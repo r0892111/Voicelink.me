aa50aba3f6abb4aa15ae693b9d600a6b1272aaa4
Finit_Solutions <113910414+r0892111@users.noreply.github.com>
2025-08-29 16:03:49 +0200

    Deleted index.ts

--- supabase/functions/odoo-auth/index.ts
+++
@@ -1,8 +0,0 @@
-@@ .. @@
-     }
-
--    // Exchange code for tokens
--    const tokenResponse = await fetch('https://accounts.odoo.com/oauth/token', {
-+    // Exchange code for tokens  
-+    const tokenResponse = await fetch('https://accounts.odoo.com/oauth2/tokeninfo', {
-       method: 'POST',
