117aaae0d333587bbbac87070ef8cd0116449cf3
Finit_Solutions <113910414+r0892111@users.noreply.github.com>
2025-10-20 10:13:01 +0200

    Updated AuthModal.tsx

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -601,14 +601,6 @@ export const AuthModal: React.FC<AuthModalProps> = ({ isOpen, onClose }) => {
                     {showPassword ? <EyeOff className="w-5 h-5" /> : <Eye className="w-5 h-5" />}
                   </button>
                 </div>
-                {isSignup && (
-                  <p className="text-xs text-gray-500 mt-1.5 flex items-center">
-                    <svg className="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd"/>
-                    </svg>
-                    Your password is encrypted and never shared with third parties.
-                  </p>
-                )}
               </div>
 
               {isSignup && (
